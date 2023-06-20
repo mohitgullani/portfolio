2c57ecb190a6c9678f1570df720a6a965e08dd6c
Mohit Gullani <67455588+mohitgullani@users.noreply.github.com>
2023-06-20 15:37:24 -0500

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,14 @@
 window.addEventListener('load',()=>{
 	let progressBars=document.querySelectorAll('.progress-bar');
 	let values=[
+		'90%',
+		'90%',
+		'80%',
+		'70%',
+		'80%',
 		'70%',
 		'70%',
-		'70%',
-		'70%',
-		'60%',
-		'50%',
-		'40%'
+		'70%'
 	];
 	progressBars.forEach((progress,index)=>{
 		progress.style.width = values[index];
